73f2d962b6cc587e2a9f0417b1f5b4bed7588489
DevoTheDev <devonfennell02@gmail.com>
2025-10-28 12:07:08 -0400

    Hopeful fixes

--- src/app/(auth)/sign-in/_services/use-mutations.ts
+++ src/app/(auth)/sign-in/_services/use-mutations.ts
@@ -1,12 +1,11 @@
+
 import { signInAction, signOutAction } from "@/app/(auth)/sign-in/_services/mutations";
 import { SignInSchema } from "@/app/(auth)/sign-in/_types/signInSchema";
 import { useMutation } from "@tanstack/react-query";
-import { toast } from "sonner";
 
 const useSignIn = () => {
   return useMutation({
     mutationFn: async (data: SignInSchema) => {
-      toast.info(JSON.stringify(data))
       await signInAction(data);
     },
   });
